5298a01e913e25eb0f6a0f8b4f4210601323e28e
XiaoV <xiaov@THINK.(none)>
2012-11-20 23:28:10 -0500

    try pack

--- destination.py
+++ destination.py
@@ -10,11 +10,11 @@ class destination:
 
     def rx_callback(self, ok, payload):
         self.__n_rcvd += 1
-        pktno = struct.unpack('!H', payload[0:2])
-        #pkt_dest = struct.unpack('!H', payload[2:4])
+        (pktno, ) = struct.unpack('!H', payload[0:2])
+        (pkt_dest, ) = struct.unpack('!H', payload[2:4])
         if ok:
             self.__n_right += 1
-        print "ok: %r \t pktno: %d \t n_rcvd: %d \t n_right: %d" % (ok, pktno, self.__n_rcvd, self.__n_right)
+        print "ok: %r \t pktno: %d \t n_rcvd: %d \t n_right: %d \t dest: %d" % (ok, pktno, self.__n_rcvd, self.__n_right,  pkt_dest)
 
 
 
