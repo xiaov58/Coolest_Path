2255ebcf203c93dde0b02cccc187eb0332b642bf
XiaoV <xiaov@THINK.(none)>
2012-12-03 13:38:46 -0500

    time sync

--- control_msg.py
+++ control_msg.py
@@ -3,7 +3,7 @@
         #1: time sync
         
         
-class time_sync_msg(control_msg):
+class time_sync_msg:
     def __init__(self, type, cnt):
         self.type = type
         control_msg.__init__(self, type)
