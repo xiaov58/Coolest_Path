3bfd65ee4cb34e5b648a9975c00f279be388ec17
XiaoV <xiaov@THINK.(none)>
2012-12-07 00:12:50 -0500

    RTS CTS

--- crn_manager.py
+++ crn_manager.py
@@ -164,7 +164,7 @@ class crn_manager:
         for i in range(len(meta_data.channels)) :
             if self.link_temp_table[next_top][i] < temprature and self.channel_mask[i] == 1 and self.neighbour_channel_mask[next_top][i] ==1:
                 self.cur_channel = i 
-                self.role.tb.set_freq(meta_data.channels[i] * 1e9)
+                self.role.tb.set_freq(meta_data.channels[i])
         if self.cur_channel == 0:
             # all channel not available
             # send routing request
