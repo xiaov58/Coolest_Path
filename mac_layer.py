f7b27b8ed2957ea48e741c68a7e31dad3f68a571
XiaoV <xiaov@THINK.(none)>
2012-12-10 23:11:05 -0500

    CSMA

--- mac_layer.py
+++ mac_layer.py
@@ -68,7 +68,7 @@ class mac_layer:
             self.crn_manager.socks_table[self.crn_manager.route[self.crn_manager.route.index(self.crn_manager.id) + 1]].send(cts_string)
             self.crn_manager.status = 0
             # give the next_hop highter priority to forward
-            time.sleep(0.05)
+            time.sleep(0.1)
             
 
             
