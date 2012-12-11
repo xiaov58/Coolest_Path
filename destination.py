8a4d3f857154cffc803225da8c87206d10ab051f
XiaoV <xiaov@THINK.(none)>
2012-12-10 23:56:33 -0500

    CSMA

--- destination.py
+++ destination.py
@@ -23,20 +23,22 @@ class destination:
         return link_number
 
     def rx_callback(self, ok, payload):
-        self.crn_manager.rx_con.acquire()
-        if self.crn_manager.status != 2:
-            self.crn_manager.rx_con.wait()
-        self.crn_manager.rx_con.release()
+#        self.crn_manager.rx_con.acquire()
+#        if self.crn_manager.status != 2:
+#            self.crn_manager.rx_con.wait()
+#        self.crn_manager.rx_con.release()
+
+        if self.crn_manager.status == 2:
             
-        (pktno, ) = struct.unpack('!H', payload[0:2])
-        (pkt_sender_id, ) = struct.unpack('!H', payload[2:4])
-        data = payload[4:]
-        if ok:
-            print "pktno: %d, sender: %d, status: %d" % (pktno, pkt_sender_id, self.crn_manager.status)
-            # only count packets from last hop
-            if self.crn_manager.route[self.crn_manager.id -1] == pkt_sender_id:
-                self.received_cnt += 1
-        else:
-            print "ok: %r \t pktno: %d \t" % (ok, pktno)
-        
+            (pktno, ) = struct.unpack('!H', payload[0:2])
+            (pkt_sender_id, ) = struct.unpack('!H', payload[2:4])
+            data = payload[4:]
+            if ok:
+                print "pktno: %d, sender: %d, status: %d" % (pktno, pkt_sender_id, self.crn_manager.status)
+                # only count packets from last hop
+                if self.crn_manager.route[self.crn_manager.id -1] == pkt_sender_id:
+                    self.received_cnt += 1
+            else:
+                print "ok: %r \t pktno: %d \t" % (ok, pktno)
+            
 
