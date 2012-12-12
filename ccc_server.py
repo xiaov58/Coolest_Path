73b8881678d44b0a3ddd4acb69d77091dba91478
XiaoV <xiaov@THINK.(none)>
2012-12-12 12:47:08 -0500

    route

--- ccc_server.py
+++ ccc_server.py
@@ -134,6 +134,7 @@ class ccc_server(threading.Thread):
                         print self.crn_manager.route
                         if ctrl_msg.route != []:
                             print "try to wake"
+                            self.crn_manager.set_best_channel()
                             self.crn_manager.process_con.acquire()
                             self.crn_manager.process_flag = 1
                             self.crn_manager.process_con.notifyAll()
