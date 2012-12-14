625c586bb3beae6cfe2a3152902c7dbbf118b4fd
XiaoV <xiaov@THINK.(none)>
2012-12-14 17:10:19 -0500

    6 nodes

--- mac_layer.py
+++ mac_layer.py
@@ -77,6 +77,7 @@ class mac_layer:
             if self.crn_manager.id != meta_data.source_id:
                 self.crn_manager.buffer_con.acquire()
                 self.crn_manager.buffer_con.wait()
+                print "buffer wake"
                 self.crn_manager.buffer_con.release()
         
         if self.crn_manager.status == 0 and len(self.buffer) != 0:
