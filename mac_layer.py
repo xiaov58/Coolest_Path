29f501a3bfb34549e0b0c7daea0974fe47727de0
XiaoV <xiaov@THINK.(none)>
2012-12-15 11:39:03 -0500

    final

--- mac_layer.py
+++ mac_layer.py
@@ -69,12 +69,12 @@ class mac_layer:
 
     def tx_run(self):            
         if self.crn_manager.status == 0 and len(self.buffer) == 0:
-            print "Loop"
-#            if self.crn_manager.id != meta_data.source_id:
-#                self.crn_manager.buffer_con.acquire()
-#                print "buffer wait at %.3f" % self.crn_manager.get_virtual_time()
-#                self.crn_manager.buffer_con.wait()
-#                self.crn_manager.buffer_con.release()
+            print "LOOP"
+            if self.crn_manager.id != meta_data.source_id:
+                self.crn_manager.buffer_con.acquire()
+                print "buffer wait at %.3f" % self.crn_manager.get_virtual_time()
+                self.crn_manager.buffer_con.wait()
+                self.crn_manager.buffer_con.release()
         
         if self.crn_manager.status == 0 and len(self.buffer) != 0:
             print "send RTS at %.3f to %d" % (self.crn_manager.get_virtual_time(), self.crn_manager.next_hop)
