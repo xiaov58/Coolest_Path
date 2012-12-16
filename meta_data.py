d1dee5c48ebd39597baf65dfa024dda51a078458
XiaoV <xiaov@THINK.(none)>
2012-12-16 11:54:59 -0500

    0.05 0.1 4

--- meta_data.py
+++ meta_data.py
@@ -3,7 +3,7 @@
 packet_size = 400           # Byte
 carrier_thredshold = -60
 bandwidth = 0.2       # MHz
-channels = [0, 2.512, 2.511, 2.515, 2.513, 2.514]
+channels = [0, 2.512, 2.511, 2.510, 2.513, 2.514]
 
 
 
